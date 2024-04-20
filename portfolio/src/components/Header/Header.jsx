89272dd123c9ae653b1b333ff40aba2558339974
Thiago <thberenger27@gmail.com>
2024-04-20 20:44:48 -0300

    React Icons, Home Page
     - Technology icons
     - new picture
     - home page style

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -8,12 +8,12 @@ import './Header.modules.css'
 const Header = () => {
   return (
     <>
-        <Navbar bg="dark" data-bs-theme="dark">
+        <Navbar data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Home</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Formações</Nav.Link>
-            <Nav.Link href="#features">Trabalhos</Nav.Link>
+            <Nav.Link href="#features">Portifólio</Nav.Link>
             <Nav.Link href="#pricing">Contato</Nav.Link>
           </Nav>
         </Container>
--- portfolio/src/routes/Home.jsx
+++ portfolio/src/routes/Home.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 
+import "../components/HomePage/HomePage.modules.css"
+
+import fotoGit from "../assets/fotoGit.jpg"
+
+// icons
+import { FaReact } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaBootstrap } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiJquery } from "react-icons/si";
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='home-container'>
+        <div className="home-container_text">
+          <h1>Thiago Berenger</h1>
+          <h4>Front-end Developer</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est labore similique neque praesentium et voluptatum error laborum expedita modi incidunt, dignissimos atque eos accusantium harum, exercitationem consequuntur eius maxime magnam!</p>
+        </div>
+          <div className='home-container_image'>
+            <img src={fotoGit} alt="foto"/>
+          </div>
+      </div>
+      
+      <div className = "technology-container">
+          <div className="technology-icons">
+              <FaHtml5/>
+              <FaCss3Alt/>
+              <IoLogoJavascript/>
+              <FaReact/>
+              <FaBootstrap/>
+              <SiJquery/>
+          </div>
+      </div>
+    </>
+    
   )
 }
 
