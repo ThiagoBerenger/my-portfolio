fd708ff163de5c372bf077445eb5b4594b40253a
Thiago <thberenger27@gmail.com>
2024-04-22 21:40:43 -0300

    Footer
     - Footer style
     -Footer content

--- portfolio/src/components/Footer/Footer.jsx
+++ portfolio/src/components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Footer.modules.css'
 
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div>FOOTER</div>
+    <>
+      <div className="footer-container">
+          <div className="footer-social">
+            <a href="" target='blank'><FaGithub /></a>
+            <a href="" target='blank'><FaLinkedin /></a>
+          </div>
+          <p>thberenger27@gmail.com</p>
+      </div>
+    </>
   )
 }
 
