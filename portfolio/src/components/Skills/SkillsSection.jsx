130f60b5a3701d201b18c57c8d42fa7ab6e5e4ee
Thiago <thberenger27@gmail.com>
2024-04-24 19:15:15 -0300

    skills page

--- portfolio/src/components/Skills/SkillsSection.jsx
+++ portfolio/src/components/Skills/SkillsSection.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import './SkillsSection.modules.css'
 
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa6";
+import { FaBootstrap } from "react-icons/fa";
+import { DiJqueryLogo } from "react-icons/di";
+
 const SkillsSection = () => {
   return (
     <>
       <div className="skill-container">
-        <h1>Habilidades</h1>
+        <h1>Tecnologias</h1>
+        <div className="techs-container">
+            <div className="tech-item">
+                <FaHtml5/>
+            </div>
+            <div className="tech-item">
+                <FaCss3Alt/>
+            </div>
+            <div className="tech-item">
+                <IoLogoJavascript/>
+            </div>
+            <div className="tech-item">
+                <FaReact/>
+            </div>
+            <div className="tech-item">
+                <FaBootstrap/>
+            </div>
+            <div className="tech-item">
+                <DiJqueryLogo/>
+            </div>
+        </div>
       </div>
     </>
   )
