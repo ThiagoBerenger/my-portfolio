9caa1a148a1c76db1cf315eacaab2557a8198455
Thiago <thberenger27@gmail.com>
2024-04-25 20:16:04 -0300

    responsive header

--- portfolio/src/components/Navbar/Header.jsx
+++ portfolio/src/components/Navbar/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react';
 
 import './Header.modules.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +9,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import { HiMiniBars3BottomRight } from "react-icons/hi2";
+
 const Header = () => {
+  const navRef = useRef()
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle('navbar_dropdown')
+  }
+
   return (
     <>
       <Navbar data-bs-theme="dark">
@@ -23,8 +31,15 @@ const Header = () => {
             <Nav.Link href="#projetos">Projetos</Nav.Link>
             <Nav.Link href="#contato">Contato</Nav.Link>
           </Nav>
+          <HiMiniBars3BottomRight className='nav-bars' onClick={showNavbar} />
         </Container>
       </Navbar>
+      <div className="hidden-navbar" ref={navRef}>
+          <div className="navbar_dropdown-link"><a href="#">Home</a></div>
+          <div className="navbar_dropdown-link"><a href="#">Habilidades</a></div>
+          <div className="navbar_dropdown-link"><a href="#">Projetos</a></div>
+          <div className="navbar_dropdown-link"><a href="#">Contato</a></div>
+      </div>
     </>
   )
 }
